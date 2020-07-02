fd5b4778f2164ba9ef9b91304ee0d203adcaf39c
KorikaDEV <46706906+KorikaDEV@users.noreply.github.com>
2020-07-02 17:28:29 +0200

    added DataControll project

---
+++ DataControll/src/main/java/minecraftutilities/datacontroll/DataControll.java
@@ -0,0 +1,17 @@
+package minecraftutilities.datacontroll;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DataControll extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
